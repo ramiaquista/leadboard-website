4ee89d977ccef3a3733e7540785d19a902c461a8
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-10-05 14:02:18 -0300

    Add plain HTML with the skeleton of the main page layout. Add style.css file with minimun styling online for placing items. Add displayList() function to display leadboard list. Add script to render all the html dynamically when the page is loaded.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,88 @@
-import _ from 'lodash';
+import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const user1 = {
+  name: 'Rami',
+  score: 100,
+};
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const user2 = {
+  name: 'Lucas',
+  score: 31,
+};
 
-  return element;
-}
+const user3 = {
+  name: 'Marcos',
+  score: 20,
+};
 
-document.body.appendChild(component());
+const user4 = {
+  name: 'Frank',
+  score: 13,
+};
+
+const leadboard = [user1, user2, user3, user4];
+
+const displayList = (leadboard, ul) => {
+  leadboard.forEach((user, index) => {
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+
+    if (index % 2 === 0) {
+      li.classList.add('white-row');
+    } else {
+      li.classList.add('grey-row');
+    }
+
+    h3.innerText = `${user.name}: ${user.score}`;
+
+    li.appendChild(h3);
+
+    ul.appendChild(li);
+  });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const listContainer = document.querySelector('.list-container');
+
+  const leadboardList = document.createElement('ul');
+
+  const listTitleContainer = document.createElement('div');
+  const listTitle = document.createElement('h2');
+  const refreshBtn = document.createElement('button');
+
+  leadboardList.classList.add('dflex');
+  leadboardList.classList.add('leadbord-list');
+  listTitleContainer.classList.add('dflex');
+  listTitleContainer.classList.add('list-header');
+  refreshBtn.classList.add('refresh-btn');
+
+  listTitle.innerText = 'Recent Scores';
+  refreshBtn.innerText = 'Refresh';
+
+  listTitleContainer.appendChild(listTitle);
+  listTitleContainer.appendChild(refreshBtn);
+
+  listContainer.appendChild(listTitleContainer);
+  listContainer.appendChild(leadboardList);
+
+  const addContainer = document.querySelector('.add-container');
+
+  const addForm = document.createElement('form');
+  const addTitle = document.createElement('h2');
+  const nameInput = document.createElement('input');
+  const scoreInput = document.createElement('input');
+  const submitBtn = document.createElement('button');
+
+  addTitle.innerText = 'Add your score';
+  nameInput.placeholder = 'Your name';
+  scoreInput.placeholder = 'Your score';
+  submitBtn.innerText = 'Submit';
+  addForm.classList.add('dflex');
+  addForm.classList.add('add-form');
+  addTitle.classList.add('add-title');
+
+  addForm.append(addTitle, nameInput, scoreInput, submitBtn);
+  addContainer.appendChild(addForm);
+
+  displayList(leadboard, leadboardList);
+});
